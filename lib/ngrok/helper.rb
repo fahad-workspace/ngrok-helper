61603e0a6954482f54f5ddf240728206763307df
Fahad Sarwar <fahad.workspace@outlook.com>
2015-05-21 19:05:16 +0530

    first commit

---
+++ lib/ngrok/helper.rb
@@ -0,0 +1,7 @@
+require "ngrok/helper/version"
+
+module Ngrok
+  module Helper
+    # Your code goes here...
+  end
+end
---
+++ lib/ngrok/helper/version.rb
@@ -0,0 +1,5 @@
+module Ngrok
+  module Helper
+    VERSION = "0.1.0"
+  end
+end
